90b8ddad7763aa0a7c62a08598e23f7f2ee0fb95
Jefferson <jeferkasta@hotmail.com>
2023-10-12 22:24:00 -0500

    Seguimiento explcacion herley

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('',views.index),
     path('about/',views.about),
-    path('hello/<str:username>',views.saludo)
+    path('saludo/<str:username>',views.saludo),
+
+    path('nosotros/',views.nosotros),
+    path('libreria/',views.libreria),
+    #path('libros/crear',views.crear, name='crear'),
+    #path('libros/editar',views.editar, name='editar')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,11 @@
 
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
 def index(request): #MELO
-    return HttpResponse("<h1>Index page<h1>")
+    return HttpResponse("<h1>Welcome<h1>")
 
 def saludo(request,username):
     return HttpResponse("<h1>Hola %s<h1>" % username)
@@ -12,4 +14,11 @@ def about(request): #MELO
     return HttpResponse("<h2>About</h2>")
 
 def nosotros(request):
-   return render(request,'paginas/nosotros.html')
+    return render(request,'paginas/nosotros.html')
+
+#CODIGO HERLEY
+def libros(request):
+    return render(request,"libros/index.htm")
+
+def libreria(request):
+    return render(request,'templates/paginas/libros/index.html')
