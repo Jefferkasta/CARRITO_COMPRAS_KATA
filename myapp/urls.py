de114765e1a40f8f3e3f2912a18156d1eb715d06
Jefferson Castañeda <jeferkasta@hotmail.com>
2023-10-18 17:21:10 -0500

    Subo trabajo en casa

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,12 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
-    path('about/',views.about),
-    path('saludo/<str:username>',views.saludo),
-
-    path('nosotros/',views.nosotros),
-    path('libreria/',views.libreria),
-    #path('libros/crear',views.crear, name='crear'),
-    #path('libros/editar',views.editar, name='editar')
+    path('', views.home)
+    
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,24 +1,14 @@
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 
 # Create your views here.
-def index(request): #MELO
-    return HttpResponse("<h1>Welcome<h1>")
-
-def saludo(request,username):
-    return HttpResponse("<h1>Hola %s<h1>" % username)
-
-def about(request): #MELO
-    return HttpResponse("<h2>About</h2>")
-
-def nosotros(request):
-    return render(request,'paginas/nosotros.html')
-
-#CODIGO HERLEY
-def libros(request):
-    return render(request,"libros/index.htm")
-
-def libreria(request):
-    return render(request,'templates/paginas/libros/index.html')
+def home(request):
+    title = 'Django course!!'
+    return render(request,'home.html',{
+        'title'   : title
+        })
+#def project(request):
+#    retunr(request)
+  
